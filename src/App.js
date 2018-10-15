e82abcbd49d2818b5f0803fddd439984a6355c39
Andrey Plis <plisandrey91@gmail.com>
2018-10-15 14:13:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DefaultContextEventListener from "../node_modules/tibbo-aggregate/src/common/context/DefaultContextEventListener";
+import Server from "./AggreGateService";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.at = new Server();
+    this.context = null;
+  }
+
+  handle = async event => {
+    event
+      .getData()
+      .rec()
+      .getValueByName("value")
+      .then(result => {
+        result
+          .rec()
+          .getValueByName("timestamp")
+          .then(re => {
+            console.log("REAL TIME: " + new Date());
+            console.log("TIMESTAMP from update: " + re);
+          });
+      });
+  };
+
+  async componentDidMount() {
+    await this.at.setUp();
+    this.context = await this.at.getContext("users.admin.devices.virtual");
+    const statusChangeListener = new DefaultContextEventListener();
+    statusChangeListener.handle = this.handle;
+    await this.context.addEventListener("updated", statusChangeListener);
+  }
+
+  async componentWillUnmount() {
+    await this.at.tearDown();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
